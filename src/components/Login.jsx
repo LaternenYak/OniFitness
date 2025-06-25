31e3e451d537cf3b9cfdd9a41a459b44fa2563b2
LaternenYak <breschchris@gmail.com>
2025-06-25 14:55:13 +0200

    Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { supabase } from "../supabase";
 import { useNavigate } from "react-router-dom";
+import "./Login.css";
 
 export default function Login(){
     const [email, setEmail] = useState ("");
